30371c7eeef965c10b59cdfb3c023ad2ba1e6c90
ricardoquinteladev <uc2020220508@student.uc.pt>
2023-03-10 19:24:46 +0000

    feat: adiciona comunicação entre SearchModule e Barrel

--- DEV/searchEngine/search/SearchModule.java
+++ DEV/searchEngine/search/SearchModule.java
@@ -1,70 +1,63 @@
 package searchEngine.search;
 
-import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.rmi.AccessException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-import searchEngine.barrel.Register;
 import searchEngine.barrel.SearchRequest;
 
-public class SearchModule implements Serializable, Remote {
+public class SearchModule {
+
+
+    /**
+     * Imprime no {@code stdin} o modo de uso do programa
+     */
+    private static void printUsage() {
+        System.out.println("Modo de uso:\nsearch {server_endpoint}");
+    }
 
 
     public static void main(String[] args) {
-        
+
         // tratamento de erros nos parametros
+        if (args.length == 0){
+            printUsage();
+            return;
+        }
+        
         if (args.length > 1){
-            System.out.print("ERRO: Parametros a mais!");
+            printUsage();
             return;
         }
 
         String rmiEndpoint = args[0];
         
-        try (Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
 
-            // tentar ligar ao RMI
-            Register server = (Register) Naming.lookup(rmiEndpoint);
+            // ligar ao server registado no rmiEndpoint fornecido
+            SearchRequest barrel = (SearchRequest) Naming.lookup(rmiEndpoint);
 
-            CopyOnWriteArrayList<String> query = new CopyOnWriteArrayList<String>();
+            ArrayList<String> query = new ArrayList<>();
+
+            query.add("Ola");
+
+            System.out.println(barrel.search(query));
+
             
-            String string;
-            SearchRequest barrel;
-
-
-            barrel = server.getBarrel(0);
-
-            while (true){
-                query.clear();
-
-                string = sc.nextLine();
-                if (string.equals("exit")) break;
-                
-                
-                System.out.print("Sending to server: " + string);
-                query.add(string);
-                
-                System.out.print(barrel + " " + barrel.search(query));
-            }
-
-
-
         } catch (NotBoundException e) {
-            System.out.print("ERRO: RMI nao esta ligado em " + rmiEndpoint + "!");
-            return;
-        } catch (MalformedURLException e) {
-            System.out.print("ERRO: nao foi possivel encontrar o RMI em " + rmiEndpoint + "!");
+            System.out.println("Erro: não existe um servidor registado no endpoint '" + rmiEndpoint + "'!");
             return;
         } catch (AccessException e) {
-            System.out.print("ERRO: nao e permitido ligar ao endpoint " + rmiEndpoint + "!");
-        } catch (RemoteException e){
-            System.out.println("ERRO: Ocorreu um erro no RMI");
-            e.printStackTrace();
+            System.out.println("Erro: Esta máquina não tem permissões para ligar ao endpoint '" + rmiEndpoint + "'!");
+            return;
+        } catch (MalformedURLException e) {
+            System.out.println("Erro: O endpoint fornecido ('" + rmiEndpoint + "') não forma um URL válido!");
+            return;
+        } catch (RemoteException e) {
+            System.out.println("Erro: Não foi possível encontrar o registo");
             return;
         }
 
