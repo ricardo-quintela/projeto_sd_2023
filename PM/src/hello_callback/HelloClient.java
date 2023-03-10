05a33557d9398f04136ef545d24f98411b2b131c
ricardoquinteladev <uc2020220508@student.uc.pt>
2023-03-10 00:29:50 +0000

    PM: adicona exemplo de comunicação RMI entre cliente-servidor e logo de seguida p2p

--- PM/src/hello_callback/HelloClient.java
+++ PM/src/hello_callback/HelloClient.java
@@ -3,8 +3,6 @@ package hello_callback;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.Scanner;
-import java.net.*;
-import java.io.*;
 
 public class HelloClient extends UnicastRemoteObject implements Hello_C_I {
 
@@ -13,7 +11,7 @@ public class HelloClient extends UnicastRemoteObject implements Hello_C_I {
 	}
 
 	public void print_on_client(String s) throws RemoteException {
-		System.out.println("> " + s);
+		System.out.println("client > " + s);
 	}
 
 	public static void main(String args[]) {
@@ -25,23 +23,28 @@ public class HelloClient extends UnicastRemoteObject implements Hello_C_I {
 		*/
  
 		try (Scanner sc = new Scanner(System.in)) {
+
 			//User user = new User();
 			Hello_S_I h = (Hello_S_I) Naming.lookup("XPTO");
 			HelloClient c = new HelloClient();
-
+	
 			h.subscribe(args[0], (Hello_C_I) c);
 			System.out.println("Client sent subscription to server");
-
+	
+	
+			Hello_C_I i;
 
 			while (true) {
 				System.out.print("> ");
 				a = sc.nextLine();
-				h.print_on_server(a);
-			}
+				i = h.print_on_server(a);
 
-		} catch (Exception e) {
-			System.out.println("Exception in main: " + e);
+				i.print_on_client("HELLO MAN");
+			}
+		} catch (Exception e){
+			e.printStackTrace();
 		}
+
 
 	}
 
--- PM/src/hello_callback/Hello_S_I.java
+++ PM/src/hello_callback/Hello_S_I.java
@@ -4,6 +4,6 @@ import java.rmi.*;
 
 public interface Hello_S_I extends Remote {
 
-	public void print_on_server(String s) throws java.rmi.RemoteException;
+	public Hello_C_I print_on_server(String s) throws java.rmi.RemoteException;
   public void subscribe(String name, Hello_C_I client) throws RemoteException;
 }
