b51b1a153162eae0e757fe2d7ef9935f6464eb78
JoaoRafaelHenriques <joao.rafael.henriques@gmail.com>
2023-03-13 15:42:12 +0000

    feat: tratamento de URLs

--- DEV/utils/tratamentoStrings.java
+++ DEV/utils/tratamentoStrings.java
@@ -1,6 +1,6 @@
 package utils;
 
-public class tratamentoStrings {
+public class TratamentoStrings {
     
     public String urlTratamento(int port, String endPoint){
         return "rmi://localhost:" + port + "/" + endPoint;
